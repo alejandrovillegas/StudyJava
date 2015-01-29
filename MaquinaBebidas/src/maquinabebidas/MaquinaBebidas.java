6ed3b5c1be3031467c4f2f07ac45a04c978f3958
alejandrovillegas <neopilatos@gmail.com>
2015-01-29 14:04:28 -0500

    Maquinas Bebidas
    Se eliminan comentarios

--- MaquinaBebidas/src/maquinabebidas/MaquinaBebidas.java
+++ MaquinaBebidas/src/maquinabebidas/MaquinaBebidas.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package maquinabebidas;
 import java.util.Scanner;
 
@@ -39,7 +35,6 @@ public class MaquinaBebidas {
            }
        }
            
-        // TODO code application logic here
     }
     
 }
